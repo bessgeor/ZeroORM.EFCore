507463a059f3c23706c02ab234eebbcca19a30b4
bessgeor <bessgeor@gmail.com>
2018-08-16 12:27:33 +0300

    Add stub interface implementation

---
+++ src/Metadata/EFCoreMetadataProvider.cs
@@ -0,0 +1,10 @@
+﻿using ZeroORM.Extensibility.Metadata;
+
+namespace ZeroORM.EFCore.Metadata
+{
+	internal class EFCoreMetadataProvider : IMetadataProvider
+	{
+		public ITableMetadata<TEntity> GetTable<TEntity>()
+			=> new EFCoreTableMetadata<TEntity>();
+	}
+}
---
+++ src/Metadata/EFCoreTableMetadata.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using HellBrick;
+using ZeroORM.Extensibility.Metadata;
+
+namespace ZeroORM.EFCore.Metadata
+{
+	internal class EFCoreTableMetadata<TEntity> : ITableMetadata<TEntity>
+	{
+		public string TableName => throw new NotImplementedException();
+
+		public string GetColumnName<TProperty>( [NoCapture] System.Linq.Expressions.Expression<Func<TEntity, TProperty>> accessor )
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
