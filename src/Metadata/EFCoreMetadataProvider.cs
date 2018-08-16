d9a5a0c1f6e725b8da13375b6a1bc6496c090521
bessgeor <bessgeor@gmail.com>
2018-08-16 17:26:43 +0300

    Implement metadata interfaces

--- src/Metadata/EFCoreMetadataProvider.cs
+++ src/Metadata/EFCoreMetadataProvider.cs
@@ -1,10 +1,25 @@
-﻿using ZeroORM.Extensibility.Metadata;
+﻿using System;
+using System.Collections.Concurrent;
+using System.Linq;
+using Microsoft.EntityFrameworkCore.Metadata;
+using ZeroORM.Extensibility.Metadata;
+using ZeroORM.Extensibility.Metadata.Exceptions;
 
 namespace ZeroORM.EFCore.Metadata
 {
 	internal class EFCoreMetadataProvider : IMetadataProvider
 	{
+		private static readonly ConcurrentDictionary<Type, IEntityType> _entities = new ConcurrentDictionary<Type, IEntityType>();
+
+		public EFCoreMetadataProvider( IModel model )
+			=> model
+				.GetEntityTypes()
+				.Select( et => _entities.GetOrAdd( et.ClrType, et ) )
+				.ToArray();
+
 		public ITableMetadata<TEntity> GetTable<TEntity>()
-			=> new EFCoreTableMetadata<TEntity>();
+			=> _entities.TryGetValue( typeof( TEntity ), out IEntityType entity )
+			? new EFCoreTableMetadata<TEntity>( entity )
+			: throw new TableNotFoundException( typeof( TEntity ) );
 	}
 }
--- src/Metadata/EFCoreTableMetadata.cs
+++ src/Metadata/EFCoreTableMetadata.cs
@@ -1,16 +1,42 @@
 ﻿using System;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
 using HellBrick;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
 using ZeroORM.Extensibility.Metadata;
+using ZeroORM.Extensibility.Metadata.Exceptions;
+using ZeroORM.Extensibility.Common.Expressions;
 
 namespace ZeroORM.EFCore.Metadata
 {
 	internal class EFCoreTableMetadata<TEntity> : ITableMetadata<TEntity>
 	{
-		public string TableName => throw new NotImplementedException();
+		private static readonly ConcurrentDictionary<PropertyInfo, string> _columnNames = new ConcurrentDictionary<PropertyInfo, string>();
 
-		public string GetColumnName<TProperty>( [NoCapture] System.Linq.Expressions.Expression<Func<TEntity, TProperty>> accessor )
+		public string TableName { get; }
+		public string SchemaName { get; }
+
+		public EFCoreTableMetadata( IEntityType efTableMetadata )
 		{
-			throw new NotImplementedException();
+			IRelationalEntityTypeAnnotations relationalMetadata = efTableMetadata.Relational();
+			TableName = relationalMetadata.TableName;
+			SchemaName = relationalMetadata.Schema;
+			efTableMetadata
+				.GetProperties()
+				.Select( prop => (prop.PropertyInfo, RelationalProps: prop.Relational()) )
+				.Select( t => _columnNames.GetOrAdd( t.PropertyInfo, t.RelationalProps.ColumnName ) )
+				.ToArray();
+		}
+
+		public string GetColumnName<TProperty>( [NoCapture] Expression<Func<TEntity, TProperty>> accessor )
+		{
+			PropertyInfo property = accessor.IsSimplePropertyAccess();
+			return _columnNames.TryGetValue( property, out string columnName )
+				? columnName
+				: throw new ColumnNotFoundException( property );
 		}
 	}
 }
