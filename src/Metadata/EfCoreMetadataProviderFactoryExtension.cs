19dc666104d1b626b07136670dfc857a7e17cfef
bessgeor <bessgeor@gmail.com>
2018-08-17 18:14:24 +0300

    Add a couple of factory exceptions

---
+++ src/Metadata/EfCoreMetadataProviderFactoryExtension.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using ZeroORM.EFCore.Metadata;
+using ZeroORM.Extensibility.Metadata;
+
+namespace System
+{
+	public static class EfCoreMetadataProviderFactoryExtension
+	{
+		public static IMetadataProvider ZeroORM( this DbContext context )
+			=> context
+			?.Model
+			?.ZeroORM()
+			?? throw new ArgumentNullException( nameof( context ) );
+
+		public static IMetadataProvider ZeroORM( this IModel model )
+			=> new EFCoreMetadataProvider( model ?? throw new ArgumentNullException( nameof( model ) ) );
+	}
+}
